d8cef819368b91d6850ea894bb14b7fe7ec4adcc
fendrirj <fendrirj@rose-hulman.edu>
2014-04-05 18:01:41 -0400

    We now time how long the network(s) take to run

--- SoftwareNeuralNetwork/cneural.c
+++ SoftwareNeuralNetwork/cneural.c
@@ -2,6 +2,10 @@
 #include <math.h>
 #include <stdio.h>
 
+#define MAX_EPOCHS 10000
+#define EPOCHS_BETWEEN_REPORTS 1000
+#define DESIRED_ERROR .001
+
 void create_training_file(int num_inputs, int num_outputs){
 	int i;
 	FILE * training_file;
@@ -11,35 +15,53 @@ void create_training_file(int num_inputs, int num_outputs){
 	training_file = fopen(file_name, "w");
 	fprintf(training_file, "%d %d %d\n", num_data, num_inputs, num_outputs);
 	for(i=0; i<num_data; i++){
-		fprintf(training_file,"%d\n", i);
-		fprintf(training_file,"%f\n", sin(i));
+		fprintf(training_file,"%f\n", ((float)i)*.1);
+		fprintf(training_file,"%f\n", sin( ((float)i)*.1 ));
 	}
 	fclose(training_file);
+}
+
+void create_ann(int num_inputs, int num_hidden, int num_outputs, char * path_to_training_file){
+	unsigned int layers []= {num_inputs, num_hidden, num_outputs};
+    struct fann * ann = fann_create_standard_array(3, layers);
+	char fann_path[100];
+
+    fann_set_activation_function_layer(ann, FANN_SIGMOID_SYMMETRIC, 1);
+    fann_set_activation_function_layer(ann, FANN_SIGMOID_SYMMETRIC, 2);
+
+    fann_train_on_file(ann, path_to_training_file , MAX_EPOCHS,
+       EPOCHS_BETWEEN_REPORTS, DESIRED_ERROR);
+
+	sprintf(fann_path, "networks/network%d.net", num_hidden);
+
+    fann_save(ann, fann_path);
+
+    fann_destroy(ann);
 }
 
 int main()
 {
 	int i;
     const float desired_error = (const float) 0.01;
-    const unsigned int max_epochs = 50000;
+    const unsigned int max_epochs = 10000;
     const unsigned int epochs_between_reports = 10;
 	int num_inputs = 1;
 	int num_outputs = 1;
+	int num_hidden = 1000;
+	int num_networks = 100;
+	char settings[100] = "networks/settings";
+	FILE * settingsFile;
 
-    unsigned int layers []= {num_inputs, 100, num_outputs};
-    struct fann * ann = fann_create_standard_array(3, layers);
-//	struct fann_train_data * train_data= fann_create_train(100, 1, 1);
-
-    fann_set_activation_function_layer(ann, FANN_SIGMOID_SYMMETRIC, 1);
-    fann_set_activation_function_layer(ann, FANN_SIGMOID_SYMMETRIC, 2);
-//    fann_set_activation_function_layer(ann, FANN_SIGMOID_SYMMETRIC, 3);
 	create_training_file(num_inputs, num_outputs);
-    fann_train_on_file(ann, "./training_file", max_epochs,
-       epochs_between_reports, desired_error);
 
-    fann_save(ann, "test.net");
+	for(i=0; i<num_networks; i++){
+		printf("Creating and training network %d\n", i+1);
+		create_ann(num_inputs, i+1, num_outputs, "training_file");
+	}
 
-    fann_destroy(ann);
+	settingsFile = fopen(settings, 'w');
+	fprintf(settingsFile, "networks:%d", num_networks);
+	fclose(settingsFile);
 
     return 0;
 }
--- SoftwareNeuralNetwork/run_network.c
+++ SoftwareNeuralNetwork/run_network.c
@@ -2,26 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <string.h>
 #include "fann.h"
 #include <math.h>
+#include <time.h>
 
-#define PATH_TO_WII_PIPE "wiiuse/wiidata"
-#define PATH_TO_NETWORK "test.net"
-#define PATH_TO_SAMPLES "wiiuse/samples/advanced/"
-#define PATH_TO_NEW_SAMPLES "samples/"
-#define NUM_INPUTS 6
-#define NUM_OUTPUTS 4
+#define PATH_TO_RESULTS "results/results.rst"
+#define PATH_TO_NETWORKS "networks/network"
+
+void run_network(char * path_to_network,int num_iterations, FILE * results, int n);
+
+void run_network(char * path_to_network,int num_iterations, FILE * results, int n){
+    FANN_EXTERNAL fann_type input[] = {M_PI};
+    FANN_EXTERNAL fann_type * output;
+    int i;
+	double time;
+//	clock_t start, end, t;
+	struct timeval start, end;
+	suseconds_t elapsed_utime;	    
+	long int elapsed_time, usec_diff, sec_diff;
+	struct fann *ann = fann_create_from_file(path_to_network);
+
+//	t = clock();
+	gettimeofday(&start, NULL);
+	for(i=0; i<num_iterations; i++){
+		output = fann_run(ann, input);
+	}
+//	t=clock() - t;
+	gettimeofday(&end, NULL);
+	usec_diff = end.tv_usec - start.tv_usec;
+	sec_diff = end.tv_sec - start.tv_sec;
+	elapsed_time = sec_diff == 0 ? end.tv_usec - start.tv_usec : (end.tv_usec+ sec_diff*1000000) - start.tv_usec ;
+
+//	fprintf(results, "%d\t%lf\t%lf\n",n,output[0], ((float)t)/CLOCKS_PER_SEC);
+	fprintf(results, "%d\t%lf\t%ld\n",n,output[0], elapsed_time);
+
+}
 
 int main(int argc, char * args[]){
     char data[50]; //Arbitrary length
 	char path[100];
-    char * token;
-    FANN_EXTERNAL fann_type input[] = {M_PI};
-    FANN_EXTERNAL fann_type * output;
-    int num_samples, i;
-	    
-	struct fann *ann = fann_create_from_file(PATH_TO_NETWORK);
-	printf("Output is: %f\n", fann_run(ann, input)[0]);
-//	output = fann_run(ann, input); //run input through network
+	char settings[100] = "networks/settings.cfg";
+	int num_networks, i, num_iterations=10000;
+	FILE * settingsFile, * results;
+	char network[100];
+
+	settingsFile = fopen(settings, "r");
+	fscanf(settingsFile, "networks:%d", &num_networks);
+	fclose(settingsFile);
+
+	results = fopen(PATH_TO_RESULTS, "w");
+	fprintf(results, "#Hidden Units\tresult\ttime (useconds)\n");
+	for(i=0; i<num_networks; i++){
+		printf("Testing network%d\n", i+1);
+		sprintf(network, "%s%d.net", PATH_TO_NETWORKS, i+1);
+		run_network(network, num_iterations, results, i+1);
+	}
+	fclose(results);
 }
