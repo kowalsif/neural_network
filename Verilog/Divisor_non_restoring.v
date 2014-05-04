eecfc60bd1e143b5d675dc1718435694f82446a4
Ian Kowalski <kowalsif@rose-hulman.edu>
2014-05-04 19:32:15 -0400

    Divider is working again without critical warnings

--- Verilog/Divisor_non_restoring.v
+++ Verilog/Divisor_non_restoring.v
@@ -29,18 +29,21 @@ module Divisor_non_restoring(
     output reg Finish
     );
     
-reg [31:0] remainder;
+reg [31:0] remainderhl;
+reg [31:0] remainderlh;
 reg [6:0] state;
 reg [31:0] divisor;
 reg rev; //tell us whether or not to set it negative. 
+wire test = remainderlh[31];
 
 always@(posedge CLOCK)
     begin
-    if (state <= 16)
+    if (state <= 15)
         begin
-        remainder[31:16] <=  remainder[31:16] - divisor[15:0]; // have to assume divisor is at most 16bit  
+        remainderlh[15:0] <= remainderhl[15:0];
+        remainderlh[31:16] =  remainderhl[31:16] - divisor[15:0]; // have to assume divisor is at most 16bit  
         end
-    if (state == 17 || state == 18) 
+    if (state == 16 || state == 17) 
         Quotient <= Quotient;
     end
 
@@ -55,38 +58,40 @@ always@(negedge CLOCK)
         rev <= Top[31]^Divisor[15];
         
         if (Top[31]==1)
-            remainder <= (~Top[31:0]) + 1'b1;
+            remainderhl = (~Top[31:0]) + 1'b1;
         else 
-            remainder <= Top;
-            
+            remainderhl = Top; 
+               
         if (Divisor[15]==1)
             divisor <= (~Divisor[31:0])+1'b1;
         else 
             divisor <= Divisor;
         end
-    else if (state == 17) //check if inverted
+    else if (state == 16) //check if inverted
         begin
         if (rev == 1)
             Quotient <= ~Quotient[31:0]+1'b1;
         state = state + 1'b1;
         Finish <= 1;
         end
-    else if (state == 18)
+    else if (state == 17)
         Finish <= 1;
         //Finish <= 1; //hold
-    else if  (remainder[31] == 0)
+    else if  (test == 0)
         begin
         Quotient <= (Quotient << 1) + 1'b1;
         state <= state + 1'b1;
+        remainderhl = remainderlh;
         //Quotient[0] <= 1;
         end
     else   
         begin
-        remainder[31:16] =  remainder[31:16] + divisor[15:0]; //restore the value
+        remainderhl[15:0] <= remainderlh[15:0];
+        remainderhl[31:16] <=  remainderlh[31:16] + divisor[15:0]; //restore the value
         Quotient <= Quotient << 1; //shift dem bits
         state <= state + 1'b1;
         end
-    remainder = remainder << 1;
+    remainderhl <= remainderhl << 1;
     end
   
 endmodule
