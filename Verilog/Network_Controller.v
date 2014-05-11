65ccc07506ba34e36903af752d099b27ef7c1396
Thriron <thriron9@gmail.com>
2014-05-11 16:45:26 -0400

    Removed activate signal from controller

--- Verilog/Network_Controller.v
+++ Verilog/Network_Controller.v
@@ -18,7 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Network_Controller(start,done, RAM_done,reset,clk,layer_sel,sum_trigger,output_sel,activate,RAM_Controll_Start);
+module Network_Controller(start,done, RAM_done,reset,clk,layer_sel,sum_trigger,output_sel,RAM_Controll_Start);
 input start, done, RAM_done, reset, clk;
 output reg [1:0] layer_sel;
 output reg sum_trigger;
@@ -37,7 +37,6 @@ always @ (state,start,RAM_done) begin
 			layer_sel <= 0;
 			sum_trigger <= 0;
 			output_sel <= 0;
-			activate <= 0;
 			RAM_Controll_Start <= 0;
 			if(start==1)
 				nextState<=1;
@@ -48,7 +47,6 @@ always @ (state,start,RAM_done) begin
 			layer_sel <= layer_sel;
 			sum_trigger <= 0;
 			output_sel <= 0;
-			activate <= activate;
 			RAM_Controll_Start <= 1;
 			nextState<=2;
 			end
@@ -56,7 +54,6 @@ always @ (state,start,RAM_done) begin
 			layer_sel <= layer_sel;
 			sum_trigger <= 0;
 			output_sel <= 0;
-			activate <= activate;
 			RAM_Controll_Start <= 0;
 			if(RAM_done==1)
 				nextState<=3;
@@ -67,7 +64,6 @@ always @ (state,start,RAM_done) begin
 			layer_sel <= layer_sel;
 			sum_trigger <= 1;
 			output_sel <= 0;
-			activate <= activate;
 			RAM_Controll_Start <= 0;
 			nextState <= 4;
 			end
@@ -75,7 +71,6 @@ always @ (state,start,RAM_done) begin
 			layer_sel <= layer_sel;
 			sum_trigger <= 0;
 			output_sel <= 0;
-			activate <= activate;
 			if(done==1)
 				nextState <= 5;
 			else
@@ -84,14 +79,8 @@ always @ (state,start,RAM_done) begin
 		5: begin
 			if(layer_sel == 2) begin
 				layer_sel <= 0;
-				activate <= num_units; //This is the first hidden unit layer, we need to make the weights bury the input signals
-			end else if(layer_sel == 1) begin
-				layer_sel <= 2;
-				activate <= num_output;
-			end else begin
-				layer_sel <= 1;
-				activate <= num_units;
-			end
+			else
+				layer_sel <= layer_sel + 1;
 			sum_trigger <= 0;
 			output_sel <= 0;
 			nextState <= 0;
@@ -100,7 +89,6 @@ always @ (state,start,RAM_done) begin
 			layer_sel <= 0;
 			sum_trigger <= 0;
 			output_sel <= 0;
-			activate <= 0;
 			nextState <= 0;
 			end
 	endcase
