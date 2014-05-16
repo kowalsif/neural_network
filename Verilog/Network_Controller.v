897cc0d4a5c86c7d10964770ed0dfa47c0ea6338
fendrirj <fendrirj@rose-hulman.edu>
2014-05-16 19:07:43 -0400

    Updated the network controller and added a tb

--- Verilog/Network_Controller.v
+++ Verilog/Network_Controller.v
@@ -18,27 +18,32 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Network_Controller(start,done, RAM_done,reset,clk,layer_sel,layer,sum_trigger,output_sel,RAM_Controll_Start);
-input start, done, RAM_done, reset, clk;
+module Network_Controller(start,done,reset,clk,layer_sel,layer,sum_trigger,RAM_Controll_Start);
+input start, done, reset, clk;
 output reg layer_sel;
-reg [1:0] layer;
+output reg [1:0] layer;
 output reg sum_trigger;
-output reg [N:0] output_sel, activate;
+//output reg [N:0] output_sel;
 output reg RAM_Controll_Start;
 
-reg state, nextState;
+reg [1:0] state, nextState;
 
 always @ (posedge clk)
 	if(reset==1) state <= 0;
 	else state <= nextState;
 
+always @ (layer)
+	if(layer == 2)
+		layer_sel <=0;
+	else
+		layer_sel <=1;
+	
 always @ (state,start,RAM_done) begin
 	case(state)
 		0: begin
 			layer <= 0;
-			layer_sel <= ?;
 			sum_trigger <= 0;
-			output_sel <= 0;
+//			output_sel <= 0;
 			RAM_Controll_Start <= 0;
 			if(start==1)
 				nextState<=1;
@@ -47,56 +52,36 @@ always @ (state,start,RAM_done) begin
 			end
 		1: begin
 			layer <= layer;
-			layer_sel <= ?;
 			sum_trigger <= 0;
-			output_sel <= 0;
+//			output_sel <= 0;
 			RAM_Controll_Start <= 1;
 			nextState<=2;
 			end
 		2: begin
 			layer <= layer;
-			layer_sel <= ?;
 			sum_trigger <= 0;
-			output_sel <= 0;
-			RAM_Controll_Start <= 0;
-			if(RAM_done==1)
-				nextState<=3;
+//			output_sel <= 0;
+			if(done==1)
+				nextState <= 3;
 			else
-				nextState<=2;
+				nextState <= 2;
 			end
 		3: begin
-			layer <= layer;
-			layer_sel <= ?;
-			sum_trigger <= 1;
-			output_sel <= 0;
-			RAM_Controll_Start <= 0;
-			nextState <= 4;
-			end
-		4: begin
-			layer <= layer;
-			layer_sel <= ?;
-			sum_trigger <= 0;
-			output_sel <= 0;
-			if(done==1)
-				nextState <= 5;
-			else
-				nextState <= 4;
-			end
-		5: begin
-			if(layer == 2) 
+			if(layer == 2)begin
 				layer <= 0;
-			else
+				nextState <= 0;
+			end
+			else begin
 				layer <= layer + 1;
-			layer_sel <= ?;
+				nextState <= 1;
+			end
 			sum_trigger <= 0;
-			output_sel <= 0;
-			nextState <= 0;
+//			output_sel <= 0;
 			end
 		default: begin
 			layer <= 0;
-			layer_sel <= ?;
 			sum_trigger <= 0;
-			output_sel <= 0;
+//			output_sel <= 0;
 			nextState <= 0;
 			end
 	endcase
